dbe3efa2aa046718255fa77010e7a8e02094b13c
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2026-02-02 08:13:39 +0530

    Add link to navigate to Add Pet page and create Add Pet component

---
+++ app/pets/new/page.tsx
@@ -0,0 +1,8 @@
+export default function AddPetPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>➕ Add Pet Page</h1>
+      <p>This page will contain the pet form.</p>
+    </div>
+  );
+}
