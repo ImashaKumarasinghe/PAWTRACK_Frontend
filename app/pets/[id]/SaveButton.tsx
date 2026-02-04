a1b74ca00c18aca84e1f4764aea4270f5009fc3c
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2026-02-04 22:17:55 +0530

    directroy mistake fixed

--- app/pets/[id]/SaveButton.tsx
+++ app/pets/[id]/SaveButton.tsx
@@ -11,7 +11,6 @@ export default function SaveButton({ petId }: { petId: number }) {
   async function handleSave() {
     setError("");
 
-    // ✅ 1) Check login when user clicks
     const token = localStorage.getItem("token");
     if (!token) {
       router.push("/login");
@@ -21,7 +20,6 @@ export default function SaveButton({ petId }: { petId: number }) {
     setLoading(true);
 
     try {
-      // ✅ 2) Send token in Authorization header
       const res = await fetch(
         `${process.env.NEXT_PUBLIC_API_URL}/pets/${petId}/save`,
         {
@@ -34,7 +32,6 @@ export default function SaveButton({ petId }: { petId: number }) {
       );
 
       if (!res.ok) {
-        // Try to read FastAPI error nicely
         let msg = "Failed to adopt pet";
         try {
           const data = await res.json();
@@ -45,10 +42,9 @@ export default function SaveButton({ petId }: { petId: number }) {
         throw new Error(msg);
       }
 
-      // ✅ 3) After saving, go back home and refresh list and go to adapted page
-      router.push(`/adopted/${petId}`);
-         router.refresh();
-
+      // ✅ go to adopted list page
+      router.push("/adopted");
+      router.refresh();
     } catch (e: any) {
       setError(e.message || "Something went wrong");
     } finally {
